cca5ab7872ddce5d4d3324b41f5e3a201de10a27
zing1230 <zing1230@hotmail.com>
2013-08-14 22:08:28 +0800

    123456

--- AppDemo/AppDemo/CustomKeywordView.h
+++ AppDemo/AppDemo/CustomKeywordView.h
@@ -17,4 +17,13 @@
 - (void)setImageName:(NSString *)imgName;
 - (void)startAnimation;
 - (void)stopAnimation;
+
+//- (void)setImageName:(NSString *)imgName;
+//- (void)startAnimation;
+//- (void)stopAnimation;
+//- (void)setImageName:(NSString *)imgName;
+//- (void)startAnimation;
+//- (void)stopAnimation;
+
+
 @end
